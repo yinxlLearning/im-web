7ec2108c03ddb4878203b41819afaa9dee75911e
yinxl <1209554955@qq.com>
2023-08-27 21:10:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {RouterLink, RouterView} from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container">
+    <RouterView/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <h2>Login</h2>
+      <form @submit.prevent="login">
+        <div class="input-group">
+          <label for="username">Username</label>
+          <input type="text" id="username" v-model="username"/>
+        </div>
+        <div class="input-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model="password"/>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
